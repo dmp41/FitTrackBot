e93631a33ac2f659a6c86d7cb1ed284f4b14817a
Dmitry Petrakov <pm-91@bk.ru>
2025-12-21 21:28:14 +0700

    commit hn_registration2

--- handlers/hn_registration.py
+++ handlers/hn_registration.py
@@ -9,6 +9,7 @@ from keyboards.ketboard_inl import menu_keyboard
 from keyboards.reply_kyboard import menu_keyboard_g
 from lexicon.lexicon_ru import name, gender, button2, birthday, height, weight, target, button3, activity, button4, \
     calories, button5, greetings
+from services.sv_registration import calculate_calories
 
 router: Router = Router()
 
@@ -48,12 +49,14 @@ async def birthday_mess(message: types.Message, state: FSMContext):
     await state.set_state(FSMFillForm.fill_height)
 
 @router.message(StateFilter(FSMFillForm.fill_height))
+          #lambda x: x.text.isdigit() and 50 <= int(x.text) <= 250)
 async def height_mess(message: types.Message, state: FSMContext):
     await state.update_data(height=message.text)
     await message.answer(text=weight)
     await state.set_state(FSMFillForm.fill_weight)
 
 @router.message(StateFilter(FSMFillForm.fill_weight))
+          #lambda x: x.text.isdigit() and 20 <= int(x.text) <= 250)
 async def weight_mess(message: types.Message, state: FSMContext):
     await state.update_data(weight=message.text)
     await message.answer(text=target, reply_markup=menu_keyboard(*button3))
@@ -70,7 +73,11 @@ async def weight_mess(callback: types.CallbackQuery, state: FSMContext):
                    F.data.in_(button4))
 async def weight_mess(callback: types.CallbackQuery, state: FSMContext):
     await state.update_data(activity=callback.data)
-    await callback.message.edit_text(text=calories, reply_markup=menu_keyboard(*button5))
+    user_dict = await state.get_data()
+    dict_calories = await calculate_calories(user_dict)
+    text = calories.format(one=dict_calories['one'], two=dict_calories['two'], three=dict_calories['three'])
+    list_button = [t.format(**dict_calories) for t in button5]
+    await callback.message.edit_text(text=text, reply_markup=menu_keyboard(*list_button))
     await state.set_state(FSMFillForm.fill_calories)
 
 
@@ -78,7 +85,9 @@ async def weight_mess(callback: types.CallbackQuery, state: FSMContext):
                    F.data.in_(button5))
 async def weight_mess(callback: types.CallbackQuery, state: FSMContext):
     await state.update_data(calories=callback.data)
-    await callback.message.answer(text=greetings, reply_markup=menu_keyboard_g)
+    user_dict = await state.get_data()
+    text = greetings.format(name=user_dict['name'])
+    await callback.message.answer(text=text, reply_markup=menu_keyboard_g)
     #await state.set_state(FSMFillForm.fill_calories)
 
 
---
+++ services/sv_registration.py
@@ -0,0 +1,7 @@
+
+
+
+async def calculate_calories(kwargs):
+
+    return {'one':1.1,'two':2.1,'three':3.1}
+
